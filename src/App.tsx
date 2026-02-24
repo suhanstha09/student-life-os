b9b6684827363f1a4e1d8c27d011e4d00b0f1fd0
suhanstha09 <scubashrestha4@gmail.com>
2026-02-24 21:19:12 +0545

    fix: remove unused Sonner import from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -23,7 +22,6 @@ const App = () => (
     <AuthProvider>
       <TooltipProvider>
         <Toaster />
-        <Sonner />
         <BrowserRouter>
           <Routes>
             <Route path="/auth" element={<Auth />} />
